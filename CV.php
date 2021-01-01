27e11e2e9b42805ed2d11286886127dbed93f44a
hungcan0611 <1851061439@e.tlu.edu.vn>
2021-01-01 21:59:42 +0700

    Them phan Services

--- CV.php
+++ CV.php
@@ -9,11 +9,41 @@ include("NavBarSection.php");
         <div class="col-md-6">
           <p class="promo-title">LOLI FOR PADDING, NOT LEWDING</p>
           <P>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores necessitatibus id quos dolores aliquid neque maiores excepturi quaerat adipisci! In impedit fugit perferendis quidem maxime reiciendis tempora quibusdam, id rem!</P>
-          <a href="#"><img src="Images/Hung.jpg" class="play-btn">Porn free here</a>
+          <a href="#"><img src="Images/Tung.jpg" class="play-btn">Porn free here</a>
+        </div>
+        <div class="col-md-6">
+          <img src="Images/Tung_chandung.jpg" class= "Tung-img">
         </div>
       </div>
     </div>
+    <img src="Images/wavy-alt-footer.png" class ="bottom-img">
 </section>
+
+<section id="services">
+    <div class="container text-center">
+      <h1 class="title">WHAT WE DO ?</h1>
+        <div class="row text-center">
+            <div class="col-md-4 services">
+              <img src="Images/GI.gif" class="services-img">
+              <h4>Growth Marketing</h4>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae doloremque labore iure sed tenetur aperiam voluptatem sunt, numquam ratione fugiat obcaecati aliquam adipisci non vel cum dolor cupiditate molestias explicabo.</p>
+            </div>
+              <div class="col-md-4 services">
+                <img src="Images/GI.gif" class="services-img">
+                <h4>Online Branding</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae doloremque labore iure sed tenetur aperiam voluptatem sunt, numquam ratione fugiat obcaecati aliquam adipisci non vel cum dolor cupiditate molestias explicabo.</p>
+                
+              </div>
+              <div class="col-md-4 services">
+                <img src="Images/GI.gif" class="services-img">
+                <h4>Animated Ads</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae doloremque labore iure sed tenetur aperiam voluptatem sunt, numquam ratione fugiat obcaecati aliquam adipisci non vel cum dolor cupiditate molestias explicabo.</p>
+                
+              </div>
+        </div>
+    </div>
+</section>
+
 <?php
 include("footer.php");
 ?>
