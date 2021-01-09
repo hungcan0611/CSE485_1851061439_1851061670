8702203c180b43ef2c0e7aa6a4beccd5d15b1036
hungcan0611 <1851061439@e.tlu.edu.vn>
2021-01-09 23:26:05 +0700

    Add about us, testimonials section

--- CV.php
+++ CV.php
@@ -44,6 +44,39 @@ include("NavBarSection.php");
     </div>
 </section>
 
+<section id="about-us">
+  <div class="container">
+    <h1 class="title text-center">Why choose us?</h1>
+      <div class="row">
+        <div class="col-md-6 about-us">
+        <p class="about-title">Why we're different</p>
+          <ul>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam officia reiciendis doloremque reprehenderit repudiandae distinctio asperiores, optio, obcaecati aperiam ratione assumenda qui, odio eius quo animi nisi sit nobis? Reiciendis?</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam officia reiciendis doloremque reprehenderit repudiandae distinctio asperiores, optio, obcaecati aperiam ratione assumenda qui, odio eius quo animi nisi sit nobis? Reiciendis?</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam officia reiciendis doloremque reprehenderit repudiandae distinctio asperiores, optio, obcaecati aperiam ratione assumenda qui, odio eius quo animi nisi sit nobis? Reiciendis?</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam officia reiciendis doloremque reprehenderit repudiandae distinctio asperiores, optio, obcaecati aperiam ratione assumenda qui, odio eius quo animi nisi sit nobis? Reiciendis?</li>
+          </ul>
+        </div>
+        <div class="col-md-6">
+          <img src="Images/GI.gif" class="img-fluid">
+        </div>
+      </div>
+  </div>
+</section>
+
+
+<section id="testimonals">
+  <div class="container">
+    <h1 class="title text-center">What Clients say</h1>
+      <div class="row">
+        <div class="col-md-5 testimonials">
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus adipisci et delectus ex nisi, deleniti aliquid dignissimos nesciunt aspernatur saepe obcaecati repudiandae minima repellat harum doloribus vel nam corporis fugiat.</p>
+          <img src="Images/Tung.jpg">
+          <p><b>Tùng</b><br>Pr0pl4y3r</p>
+        </div>
+      </div>
+  </div>
+</section>
 <?php
 include("footer.php");
 ?>
